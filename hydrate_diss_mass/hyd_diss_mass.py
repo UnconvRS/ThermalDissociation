bfcb93faaae96a5d05d061e24580bded91c7577c
Meisam <me.adibifard@gmail.com>
2024-08-20 00:52:53 -0500

    new commit

--- hydrate_diss_mass/hyd_diss_mass.py
+++ hydrate_diss_mass/hyd_diss_mass.py
@@ -10,60 +10,60 @@ from tqdm import tqdm
 import ntpath
 
 # import in-house modules
-from modules.units import units 
-from modules.settings import simSettings
-from modules.simulation import  simulation
+from hydrate_diss_mass.modules.units import units
+from hydrate_diss_mass.modules.settings import simSettings
+from hydrate_diss_mass.modules.simulation import simulation
 
 # Here the code begins
 # set type of input
-toi=1 # 1:cli, 2: in source code
-if toi==1:
+toi = 1  # 1:cli, 2: in source code
+if toi == 1:
     # Get input form cli
-    parser=argparse.ArgumentParser();
+    parser = argparse.ArgumentParser();
     # required arguments
-    parser.add_argument("--SimDir","-sd",type=str,required=True,help='The path to the simulation directory where all restart folders are located (required)')
-    parser.add_argument("--NucellPerTraj","-nupt",type=int,required=True, help="""The depth of each simulation trajectory, which is the number of unit-cells
+    parser.add_argument("--SimDir", "-sd", type=str, required=True,
+                        help='The path to the simulation directory where all restart folders are located (required)')
+    parser.add_argument("--NucellPerTraj", "-nupt", type=int, required=True, help="""The depth of each simulation trajectory, which is the number of unit-cells
         by which each simulation trajectory is extended in the direction normal to its plane (required)""")
-    parser.add_argument("--Simunits","-su",nargs=2,required=True,help='The units for the simulation data in the following format [time,length]')
-    parser.add_argument("--Outunits","-ou",nargs=2,required=True,help='The units for the output data in the following format [time,length]')
-    parser.add_argument("--tinfo","-ti",nargs=2,required=True,help='Gets dt (fs), and dump_freq (#time-steps) respectively (required)')
+    parser.add_argument("--Simunits", "-su", nargs=2, required=True, help='The units for the simulation data in the following format [time,length]')
+    parser.add_argument("--Outunits", "-ou", nargs=2, required=True, help='The units for the output data in the following format [time,length]')
+    parser.add_argument("--tinfo", "-ti", nargs=2, required=True, help='Gets dt (fs), and dump_freq (#time-steps) respectively (required)')
 
     # optional arguments
-    parser.add_argument("--NumImagesSkipped","-si",type=int,required=False,default=1, help='The number of images skipped to speed up the calculations (optional), default=1')
-    
+    parser.add_argument("--NumImagesSkipped", "-si", type=int, required=False, default=1,
+                        help='The number of images skipped to speed up the calculations (optional), default=1')
+
     # parse the arguments
-    args=parser.parse_args()
-    
-    simDir=str(pathlib.PurePosixPath(args.SimDir))
+    args = parser.parse_args()
 
-    dt=float(args.tinfo[0])
-    dump_freq=int(args.tinfo[1])
-    
-    NumCrystPerLayer=args.NucellPerTraj
-    numImagesSkipped=args.NumImagesSkipped
+    simDir = str(pathlib.PurePosixPath(args.SimDir))
 
-    SimUnits=units(args.Simunits[0],args.Simunits[1])
-    OutUnits=units(args.Outunits[0],args.Outunits[1])
+    dt = float(args.tinfo[0])
+    dump_freq = int(args.tinfo[1])
+
+    NumCrystPerLayer = args.NucellPerTraj
+    numImagesSkipped = args.NumImagesSkipped
+
+    SimUnits = units(args.Simunits[0], args.Simunits[1])
+    OutUnits = units(args.Outunits[0], args.Outunits[1])
 else:
-    simDir=r'inputs\trajectories'
+    simDir = r'inputs\trajectories'
 
-    dt=10 #fs (simulation time-step)
-    dump_freq=20000 # number of time-steps per LAMMPS trajectory dump
-    
-    NumCrystPerLayer=2
-    numImagesSkipped=100
+    dt = 10  # fs (simulation time-step)
+    dump_freq = 20000  # number of time-steps per LAMMPS trajectory dump
 
-    SimUnits=units('fs','A')
-    OutUnits=units('s','A')
+    NumCrystPerLayer = 2
+    numImagesSkipped = 100
 
-    
+    SimUnits = units('fs', 'A')
+    OutUnits = units('s', 'A')
 
-SimUnits.update_convfactors()  
-DynamicData={"dt":dt,"dump_freq":dump_freq}
+SimUnits.update_convfactors()
+DynamicData = {"dt": dt, "dump_freq": dump_freq}
 # set the global settings 
-simulation_settings=simSettings(numImagesSkipped,NumCrystPerLayer,DynamicData,SimUnits,OutUnits)
+simulation_settings = simSettings(numImagesSkipped, NumCrystPerLayer, DynamicData, SimUnits, OutUnits)
 # instantiate a simulation case
-SimCase=simulation(simDir)
+SimCase = simulation(simDir)
 # process the images and generate mass(hydrate)-time data
 SimCase.run()
 
